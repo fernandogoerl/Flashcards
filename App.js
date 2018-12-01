4c878147c20b929d80c040d8161a373ddb296676
Fernando Goerl <fernandogoerl@gmail.com>
2018-12-01 19:19:33 -0200

    feat: create basic app structure

--- App.js
+++ App.js
@@ -1,21 +1,103 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, StatusBar } from 'react-native';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation'
+import { Constants } from 'expo'
+import { Decks, NewDeck, DeckDetail, AddCard, Quiz } from './components'
+import { blue, white } from './constants/colors'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+
+function FlashcardsStatusBar ({backgroundColor, ...props}) {
+	return (
+		<View style={{backgroundColor, height: Constants.StatusBarHeight}}>
+			<StatusBar translucent backgroundColor={backgroundColor} {...props} />
+		</View>
+	)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Tabs = createBottomTabNavigator({
+	Decks: {
+		screen: Decks,
+		navigationOptions: {
+			tabBarLabel: 'Decks',
+			tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='cards' size={30} color={tintColor} />
+		}
+	},
+	NewDeck: {
+		screen: NewDeck,
+		navigationOptions: {
+			tabBarLabel: 'New Deck',
+			tabBarIcon: ({ tintColor }) => <MaterialCommunityIcons name='library-plus' size={30} color={tintColor} />
+		}
+	}
+},
+{
+	navigationOptions: {
+		header: null
+	},
+	tabBarOptions: {
+		activeTintColor: blue,
+		style: {
+			height: 56,
+			backgroundColor:  white,
+			shadowColor: 'rgba(0, 0, 0, 0.24)',
+			shadowOffset: {
+				width: 0,
+				height: 3
+			},
+			shadowRadius: 6,
+			shadowOpacity: 1
+		}
+	}
+})
+
+const MainNavigator = createStackNavigator({
+	Home: {
+		screen: Tabs,
+		navigationOptions: {
+			title: 'Flashcards',
+			headerTintColor: white,
+			headerStyle: {
+				backgroundColor: blue
+			}
+		}
+	},
+	DeckDetail: {
+		screen: DeckDetail,
+		navigationOptions: {
+			headerTintColor: white,
+			headerStyle: {
+				backgroundColor: blue
+			}
+		}
+	},
+	AddCard: {
+		screen: AddCard,
+		navigationOptions: {
+			headerTintColor: white,
+			headerStyle: {
+				backgroundColor: blue
+			}
+		}
+	},
+	Quiz: {
+		screen: Quiz,
+		navigationOptions: {
+			headerTintColor: white,
+			headerStyle: {
+				backgroundColor: blue
+			}
+		}
+	}
+})
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<View style={{flex:1}}>
+				<FlashcardsStatusBar backgroundColor={blue} barStyle='light-content' />
+				<MainNavigator />
+			</View>
+		);
+	}
+}
